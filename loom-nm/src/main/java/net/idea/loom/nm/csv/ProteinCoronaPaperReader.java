f940625500bed709c301af2d853206dd8698fa89
ngn <jeliazkova.nina@gmail.com>
2014-05-16 13:42:57 +0300

    Reader for https://github.com/ideaconsult/Protein_Corona CSV file

---
+++ loom-nm/src/main/java/net/idea/loom/nm/csv/ProteinCoronaPaperReader.java
@@ -0,0 +1,86 @@
+package net.idea.loom.nm.csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openscience.cdk.exception.CDKException;
+
+import ambit2.base.data.study.Params;
+import ambit2.base.data.study.Protocol;
+import ambit2.base.data.study.ProtocolApplication;
+import ambit2.core.io.IteratingDelimitedFileReaderComplexHeader;
+import ambit2.core.io.StringArrayHeader;
+
+public class ProteinCoronaPaperReader extends IteratingDelimitedFileReaderComplexHeader<StringArrayHeader> {
+
+	public ProteinCoronaPaperReader(InputStream in)
+			throws UnsupportedEncodingException, CDKException {
+		super(in);
+		setNumberOfHeaderLines(6);
+	}
+	public ProteinCoronaPaperReader(Reader reader)  throws CDKException {
+		super(reader);
+		setNumberOfHeaderLines(6);
+	}
+
+	@Override
+	protected void processHeader(BufferedReader in) {
+		for (int i=0; i < getNumberOfHeaderLines();i++) {
+			processHeader(in,i);		
+		}	
+	}
+	
+	protected void processHeader(BufferedReader in, int nline) {
+		try {
+			
+			String line = in.readLine();
+			while (line.startsWith(commentChar)) {
+				processComment(line);
+				line = in.readLine();
+			}
+			String[] tokens = StringUtils.splitPreserveAllTokens(line,new String(format.getFieldDelimiter()));
+			int col = 0;
+			for (String token : tokens) {
+				if (nline==0)
+					addHeaderColumn(token);
+				else {
+					StringArrayHeader column = getHeaderColumn(col);
+					column.setValue(nline, token);
+				}
+				col++;
+			}
+			//no SMILES expected, if needed get code from super class
+			if (nline==0)
+				values = new Object[getNumberOfColumns()];
+			
+			
+		} catch (IOException x) {
+			logger.log(Level.SEVERE,x.getMessage(),x); 
+		}
+	}
+    
+	@Override
+	protected String getSmilesHeader(int index) {
+		return null;
+	}
+
+	@Override
+	protected StringArrayHeader createPropertyByColumnName(String name) {
+		return new StringArrayHeader(getNumberOfHeaderLines(),name) {
+			@Override
+			protected ProtocolApplication<Protocol, Params, String, Params, String> getExperiment(Protocol protocol) {
+				ProtocolApplication<Protocol, Params, String, Params, String> exp = new ProtocolApplication<Protocol, Params, String, Params, String>(protocol);
+				exp.setReferenceYear("2014");
+				exp.setReference("Protein Corona Fingerprinting Predicts the Cellular Interaction of Gold and Silver Nanoparticles");
+				return exp;
+			}
+		};
+	}
+	
+}
+
---
+++ loom-nm/src/main/java/net/idea/loom/nm/csv/ProteinCoronaSubstanceReader.java
@@ -0,0 +1,61 @@
+package net.idea.loom.nm.csv;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.base.data.LiteratureEntry;
+import ambit2.base.data.SubstanceRecord;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.core.io.RawIteratingWrapper;
+import ambit2.core.io.StringArrayHeader;
+
+public class ProteinCoronaSubstanceReader  extends RawIteratingWrapper<ProteinCoronaPaperReader> {
+
+	public ProteinCoronaSubstanceReader(ProteinCoronaPaperReader reader) {
+		super(reader);
+	}
+	@Override
+	protected IStructureRecord createStructureRecord() {
+		return new SubstanceRecord();
+	}
+
+
+	@Override
+	protected Object transform(Object o) {
+		if (o instanceof IAtomContainer) try {
+
+			r.clear();
+			r.setFormat("SDF");
+			java.util.Set<Object> keys = ((IAtomContainer)o).getProperties().keySet();
+			Iterator  i = keys.iterator();
+			while (i.hasNext()) {
+				Object key = i.next();
+				if (key instanceof StringArrayHeader) {
+					((StringArrayHeader)key).assign((SubstanceRecord)r, ((IAtomContainer)o).getProperties().get(key));
+				}
+				//System.out.println(key.getClass().getName() + " : " + key);
+//				r.setProperty(key, ((IAtomContainer)o).getProperties().get(key));
+			}
+			if (((SubstanceRecord)r).getCompanyUUID()==null)
+				((SubstanceRecord)r).setCompanyUUID("PRCR-"+UUID.randomUUID());
+			((IAtomContainer)o).getProperties().clear();
+
+
+			r.setContent(writer.process((IAtomContainer)o));
+			Object ref = ((IAtomContainer)o).getProperty("REFERENCE");
+			if (ref instanceof LiteratureEntry)
+				r.setReference((LiteratureEntry)ref);
+			else r.setReference(getReference());
+			
+			return r;  
+		} catch (Exception x) {
+			r.clear();
+			r.setFormat("SDF");
+			r.setContent(null);
+			r.setReference(getReference());
+			return r;  
+		} else return o;
+	}
+}
