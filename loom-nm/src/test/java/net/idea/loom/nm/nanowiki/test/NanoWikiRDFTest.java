20f84ca42a4abfbc6b1f654834e40c2c4f5f3379
vedina <jeliazkova.nina@gmail.com>
2015-01-26 22:25:00 +0200

    1.0.8-SNAPSHOT

--- loom-nm/src/test/java/net/idea/loom/nm/nanowiki/test/NanoWikiRDFTest.java
+++ loom-nm/src/test/java/net/idea/loom/nm/nanowiki/test/NanoWikiRDFTest.java
@@ -15,40 +15,47 @@ import ambit2.base.interfaces.IStructureRecord;
 
 public class NanoWikiRDFTest {
 
-    @Test
-    public void test() throws Exception {
-	NanoWikiRDFReader reader = null;
-	int records = 0;
-	try {
-	    reader = new NanoWikiRDFReader(new InputStreamReader(new FileInputStream(new File(
-		    "D:/src-other/nanowiki/backup_public.rdf")), "UTF-8"));
-	    while (reader.hasNext()) {
-		IStructureRecord record = reader.nextRecord();
-		Assert.assertTrue(record instanceof SubstanceRecord);
-		// if (((SubstanceRecord)record).getMeasurements()!=null)
-		// System.out.println(((SubstanceRecord)record).getMeasurements());
-		SubstanceRecord material = (SubstanceRecord) record;
-		System.out.print(material.getCompanyName());
-		System.out.print("\t");
-		System.out.print(material.getReference());
-		System.out.print("\t");
-		if (material.getMeasurements() == null)
-		    System.out.print("\t");
-		else {
-		    System.out.print(material.getMeasurements().size());
-		    System.out.print("\t");
-		    for (ProtocolApplication papp : material.getMeasurements()) {
-			System.out.print("Ref " + papp.getReference());
-			System.out.print("\t");
-		    }
-		}
-		System.out.println();
+	@Test
+	public void test() throws Exception {
+		NanoWikiRDFReader reader = null;
+		int records = 0;
+		try {
+			reader = new NanoWikiRDFReader(new InputStreamReader(
+					new FileInputStream(new File(
+							"D:/nina/ChemicalData/enm/backup_25012015.rdf")),
+					"UTF-8"));
+			while (reader.hasNext()) {
+				IStructureRecord record = reader.nextRecord();
+				Assert.assertTrue(record instanceof SubstanceRecord);
+				// if (((SubstanceRecord)record).getMeasurements()!=null)
+				// System.out.println(((SubstanceRecord)record).getMeasurements());
+				SubstanceRecord material = (SubstanceRecord) record;
+				System.out.print(material.getCompanyName());
+				System.out.print("\t");
+				System.out.print(material.getReference().getName());
+				System.out.print("\t");
+				System.out.print(material.getReference().getTitle());
+				System.out.print("\t");		
+				System.out.print(material.getReference().getURL());
+				System.out.print("\t");					
+				if (material.getMeasurements() == null) {
+					System.out.print(0);
+					System.out.print("\t");
+				} else {
+					System.out.print(material.getMeasurements().size());
+					System.out.print("\t");
+					for (ProtocolApplication papp : material.getMeasurements()) {
+						System.out.print("Ref " + papp.getReference());
+						System.out.print("\t");
+					}
+				}
+				System.out.println();
 
-		records++;
-	    }
-	} finally {
-	    reader.close();
+				records++;
+			}
+		} finally {
+			reader.close();
+		}
+		System.out.println(records);
 	}
-	System.out.println(records);
-    }
 }
