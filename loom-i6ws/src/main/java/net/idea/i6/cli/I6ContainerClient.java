82d48c9676b66fa5736538453c33da43056edc8d
ngn <jeliazkova.nina@gmail.com>
2017-07-02 08:13:48 +0300

    export query working

--- loom-i6ws/src/main/java/net/idea/i6/cli/I6ContainerClient.java
+++ loom-i6ws/src/main/java/net/idea/i6/cli/I6ContainerClient.java
@@ -32,6 +32,28 @@ public class I6ContainerClient extends IUCLIDAbstractClient<I6Credentials> imple
 		super(httpclient, baseURL, token);
 	}
 
+	final static String MediaTypeI6Job = "application/vnd.iuclid6.ext+json; type=iuclid6.Iuclid6Job";
+	final static String MediaTypeI6Export = "application/vnd.iuclid6.ext+json; type=iuclid6.FullExport";
+
+	protected enum _I6JOBSTATUS {
+		QUEUED {
+			@Override
+			public boolean isFinal() {
+				return false;
+			}
+		},
+		IN_PROGRESS {
+			@Override
+			public boolean isFinal() {
+				return false;
+			}
+		},
+		SUCCEEDED, FAILED, CANCELED;
+		public boolean isFinal() {
+			return true;
+		}
+	}
+
 	@Override
 	protected List<IIdentifiableResource<IIdentifier>> get(IIdentifier identifier, String mediaType, String... params)
 			throws RestException, IOException {
@@ -43,8 +65,8 @@ public class I6ContainerClient extends IUCLIDAbstractClient<I6Credentials> imple
 				httpPOST.addHeader(header);
 
 		// httpPOST.addHeader("Accept-Encoding", "gzip,deflate");
-		httpPOST.addHeader("Content-type", "application/vnd.iuclid6.ext+json; type=iuclid6.FullExport");
-		httpPOST.addHeader("Accept", "application/vnd.iuclid6.ext+json; type=iuclid6.Iuclid6Job");
+		httpPOST.addHeader("Content-type", MediaTypeI6Export);
+		httpPOST.addHeader("Accept", MediaTypeI6Job);
 
 		HttpEntity content = new StringEntity("{}", "UTF-8");
 		httpPOST.setEntity(content);
@@ -55,36 +77,47 @@ public class I6ContainerClient extends IUCLIDAbstractClient<I6Credentials> imple
 			HttpResponse response = getHttpClient().execute(httpPOST);
 			HttpEntity entity = response.getEntity();
 
-			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK || response.getStatusLine().getStatusCode() == HttpStatus.SC_ACCEPTED) {
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK
+					|| response.getStatusLine().getStatusCode() == HttpStatus.SC_ACCEPTED) {
 				try (InputStream in = entity.getContent()) {
 					// return processPayload(in, identifier.toString());
 					String resultURI = null;
 					ObjectMapper m = new ObjectMapper();
 					JsonNode node = m.readTree(in);
 					String uri = node.get("uri").asText();
-					if (!SUCCEEDED.equals(node.get("status"))) {
-						if (polling(uri))
+					_I6JOBSTATUS jobstatus = null;
+					try {
+						jobstatus = _I6JOBSTATUS.valueOf(node.get("status").asText());
+					} catch (Exception x) {
+						logger.log(Level.WARNING, x.getMessage());
+						return null;
+					}
+					if (!jobstatus.isFinal()) {
+						jobstatus = polling(uri);
+						if (_I6JOBSTATUS.SUCCEEDED.equals(jobstatus))
 							resultURI = uri + "/result";
-						else
+						else {
 							resultURI = null;
+						}
 					} else
 						resultURI = uri + "/result";
 
 					if (resultURI == null)
 						return Collections.emptyList();
 
-					HttpGet result = new HttpGet(uri);
+					HttpGet result = new HttpGet(String.format("%s/system%s/result", baseURL, uri));
 					if (headers != null)
 						for (Header header : headers)
 							result.addHeader(header);
 					HttpResponse resultresponse = getHttpClient().execute(result);
-					try (InputStream inr = resultresponse.getEntity().getContent()) {
+					if (resultresponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
+						try (InputStream inr = resultresponse.getEntity().getContent()) {
 
-						return processPayload(inr, identifier.toString());
-					}
-
+							return processPayload(inr, identifier.toString());
+						}
+					
 				} catch (Exception x) {
-					x.printStackTrace();
+					logger.log(Level.WARNING, x.getMessage());
 				}
 
 			} else if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND) {
@@ -99,12 +132,10 @@ public class I6ContainerClient extends IUCLIDAbstractClient<I6Credentials> imple
 		}
 	}
 
-	private final static String SUCCEEDED = "SUCCEEDED";
-
-	protected boolean polling(String uri) throws RestException, IOException {
+	protected _I6JOBSTATUS polling(String uri) throws RestException, IOException {
 		ObjectMapper m = new ObjectMapper();
-		HttpGet polling = new HttpGet(String.format("%s/system%s",baseURL,uri));
-		polling.addHeader("Accept", "accept:application/vnd.iuclid6.ext+json; type=iuclid6.Iuclid6Job");
+		HttpGet polling = new HttpGet(String.format("%s/system%s", baseURL, uri));
+		polling.addHeader("Accept", MediaTypeI6Job);
 		if (headers != null)
 			for (Header header : headers)
 				polling.addHeader(header);
@@ -118,18 +149,28 @@ public class I6ContainerClient extends IUCLIDAbstractClient<I6Credentials> imple
 			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 				try (InputStream in = response.getEntity().getContent()) {
 					JsonNode node = m.readTree(in);
-					if (SUCCEEDED.equals(node.get("status")))
-						return true;
+					_I6JOBSTATUS jobstatus = null;
+					try {
+						jobstatus = _I6JOBSTATUS.valueOf(node.get("status").asText());
+						if (jobstatus.isFinal())
+							return jobstatus;
+						else
+							continue;
+					} catch (Exception x) {
+						logger.log(Level.WARNING, x.getMessage());
+						return null;
+					}
+
 				}
 			}
 			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_UNPROCESSABLE_ENTITY)
-				return false;
+				return _I6JOBSTATUS.FAILED;
 			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_INTERNAL_SERVER_ERROR)
-				return false;
+				return _I6JOBSTATUS.FAILED;
 			logger.log(Level.WARNING, response.getStatusLine().toString());
 			break;
 		}
-		return false;
+		return _I6JOBSTATUS.FAILED;
 	}
 
 	@Override
